eccca09ce874918b2fdfac3585a7540f5514ca35
Julian Simioni <julian.simioni@gmail.com>
2013-03-02 14:16:50 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloHero::Application.config.secret_token = '7def7ff963afcfff1ebaab7573401cb047b2e7250b226c54aec0b6a903b1060ab0aa4f6576a3243a4f7bb52329a522f2b237808569efc79330115a42800e8c3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HelloHero::Application.config.session_store :cookie_store, key: '_HelloHero_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HelloHero::Application.config.session_store :active_record_store
