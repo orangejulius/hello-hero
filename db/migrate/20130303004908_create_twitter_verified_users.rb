debc64dc59ab31ce1fb8d92b6357334ba0961d48
Julian Simioni <julian.simioni@gmail.com>
2013-03-02 17:48:46 -0800

    Change field from string to text

--- db/migrate/20130303004908_create_twitter_verified_users.rb
+++ db/migrate/20130303004908_create_twitter_verified_users.rb
@@ -2,7 +2,7 @@ class CreateTwitterVerifiedUsers < ActiveRecord::Migration
   def change
     create_table :twitter_verified_users do |t|
       t.integer :twitter_id
-      t.string :data
+      t.text :data
     end
   end
 end
