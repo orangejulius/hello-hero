bdf02e0afde9d477f394497f7b7f965e29db3440
Julian Simioni <julian.simioni@gmail.com>
2013-03-02 21:36:21 -0800

    Add accessors for various size images

--- app/models/twitter_verified_user.rb
+++ app/models/twitter_verified_user.rb
@@ -1,6 +1,18 @@
 class TwitterVerifiedUser < ActiveRecord::Base
   attr_accessible :data, :twitter_id, :profile_image_url, :screen_name, :name
 
+  def big_profile_image_url
+    profile_image_url.sub('normal', 'bigger')
+  end
+
+  def mini_profile_image_url
+    profile_image_url.sub('normal', 'mini')
+  end
+
+  def original_profile_image_url
+    profile_image_url.sub('_normal', '')
+  end
+
   def highest_bid
   end
 end
