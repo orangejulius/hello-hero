3c3e845beb429dcdf05eeb3017afe9b40f6e0022
Han <szu.han.chang@gmail.com>
2013-03-03 15:51:31 -0800

    Add dynamic titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def yield_for(content_sym, default)
+    output = content_for(content_sym)
+    output = default if output.blank?
+    if content_sym == :title and output != 'HelloHero'
+      output = output + ' | HelloHero'
+    end
+    output
+  end
 end
