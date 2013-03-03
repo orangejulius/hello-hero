87992b7a937c1a81658756ec1068f87371dfca35
Julian Simioni <julian.simioni@gmail.com>
2013-03-02 17:15:05 -0800

    Remove timestamps from TwitterVerifiedUser table

--- db/migrate/20130303004908_create_twitter_verified_users.rb
+++ db/migrate/20130303004908_create_twitter_verified_users.rb
@@ -3,8 +3,6 @@ class CreateTwitterVerifiedUsers < ActiveRecord::Migration
     create_table :twitter_verified_users do |t|
       t.integer :twitter_id
       t.string :data
-
-      t.timestamps
     end
   end
 end
