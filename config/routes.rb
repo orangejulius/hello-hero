3a2ac32efb57d28d2963212367b34bb2dcd77be2
Han <szu.han.chang@gmail.com>
2013-03-02 16:20:50 -0800

    Add pages controller and nav partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 HelloHero::Application.routes.draw do
+  get "pages/index"
+
+  get "pages/about"
+
+  get "pages/terms"
+
+  get "pages/privacy"
+
+  get "pages/contact"
+
   ActiveAdmin.routes(self)
 
   devise_for :users
@@ -52,7 +62,7 @@ HelloHero::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
