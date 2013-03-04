c13359ee843758908cce9274920585f6fd7d8816
Han <szu.han.chang@gmail.com>
2013-03-03 21:41:08 -0800

    Fix catastrophic bug

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -10,11 +10,15 @@ class HeroesController < ApplicationController
       redirect_to root_path, :notice => "Sorry... #{params[:name]} isn't a person in our system at this time. <a href='mailto:hi@hellohero.co'>Contact us</a> to add them!" and return
     end
 
-    # Find intersection of hero's bids and user's bids,
-    # grab (what should be) the only entry / aka first entry,
-    # if it doesn't exist then create a new bid.
-    # This is used to either allow the user to place a new bid for the first time,
-    # or update their existing bid (but only increase it).
-    @bid = (@hero.bids & current_user.bids).first || Bid.new
+    if current_user.nil?
+      @bid = Bid.new
+    else
+      # Find intersection of hero's bids and user's bids,
+      # grab (what should be) the only entry / aka first entry,
+      # if it doesn't exist then create a new bid.
+      # This is used to either allow the user to place a new bid for the first time,
+      # or update their existing bid (but only increase it).
+      @bid = (@hero.bids & current_user.bids).first || Bid.new
+    end
   end
 end
