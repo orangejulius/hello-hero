1a5e0633baf1a4a3009d1c9ad7ed4b516de37742
Han <szu.han.chang@gmail.com>
2013-03-03 20:20:14 -0800

    Make bid updating a reality

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,5 +1,4 @@
 class BidsController < ApplicationController
-
   def create
     @bid = Bid.new(params["bid"])
     respond_to do |format|
@@ -12,4 +11,28 @@ class BidsController < ApplicationController
     end
   end
 
+  def update
+    @bid = Bid.find(params['id'])
+    new_amount = params['bid']['amount'].to_d || 0.0
+
+    # This should never happen.
+    if @bid.nil?
+      flash[:error] = "You must have a bid to update it!"
+      redirect_to(:back) and return
+    end
+
+    if @bid.amount >= new_amount
+      flash[:error] = "Your new bid must be higher than your current bid of #{view_context.number_to_currency(@bid.amount)}."
+      redirect_to(:back) and return
+    end
+
+    @bid.amount = new_amount
+    if @bid.save!
+      flash[:success] = "Your bid has been updated to #{view_context.number_to_currency(params['bid']['amount'])}!"
+    else
+      flash[:error] = 'There was an error updating your bid.'
+    end
+
+    redirect_to(:back)
+  end
 end
--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -5,10 +5,16 @@ class HeroesController < ApplicationController
 
   def leaderboard
     @hero = TwitterVerifiedUser.find_by_name(params[:name])
-    @bids = @hero.bids
-    @bid = Bid.new
+
+    # Find intersection of hero's bids and user's bids,
+    # grab (what should be) the only entry / aka first entry,
+    # if it doesn't exist then create a new bid.
+    # This is used to either allow the user to place a new bid for the first time,
+    # or update their existing bid (but only increase it).
+    @bid = (@hero.bids & current_user.bids).first || Bid.new
+
     unless @hero
-      flash[:error] = "No one named #{params[:name]} was found"
+      flash[:error] = "No one named #{params[:name]} was found..."
       redirect_to :action => :index
     end
   end
